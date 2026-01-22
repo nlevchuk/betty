c831ba42636f254cad9d5793ae4ad2ba2831a5bc
Nikita Levchuk <code@levch.uk>
2026-01-22 10:06:17 +0100

    fix: use the absolute path when running the bash script outside of the working directory

--- prompt.ts
+++ prompt.ts
@@ -3,5 +3,6 @@ import { readFileSync } from 'node:fs'
 const FILENAME = '.prompt';
 
 export function readPrompt(): string {
-  return readFileSync(FILENAME, { encoding: 'utf8' });
+  const filePath = new URL(FILENAME, import.meta.url);
+  return readFileSync(filePath, { encoding: 'utf8' });
 }
